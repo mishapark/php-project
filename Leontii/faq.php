a650bf1be874d8cf4f91f99c8f0c3bbdbf07757b
Leontii Polovinko <leontii_polovinko@epam.com>
2021-10-12 03:50:53 +0300

    add php files

---
+++ Leontii/faq.php
@@ -0,0 +1,35 @@
+<?php 
+include_once '../header.php'; 
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+    <link rel="stylesheet" a href="css\faq.css">
+</head>
+
+<body>
+    <div class="box">
+        <p class="heading">FAQs</p>
+        <div class="faqs">
+            <details>
+                <summary>First question?</summary>
+                <p class="text">First question answer.</p>
+            </details>
+            <details>
+                <summary>Second question?</summary>
+                <p class="text">Second question answer.</p>
+            </details>
+            <details>
+                <summary>Third question?</summary>
+                <p class="text">Third question answer.</p>
+            </details>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php 
+include_once '../footer.php'; 
+?> 
